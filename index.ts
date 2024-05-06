4847f677c8c9fd71550afc0e9b3662e8e6479d1c
malaika khaliq lodhi <163471373+malaikakhaliqlodhi@users.noreply.github.com>
2024-05-06 10:29:22 -0700

    cli game done

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random()* 10 + 1)
+const answers= await inquirer.prompt([
+   {name: "userguessnumber",
+    type: 'number',
+    message:"please guess a number between 1-10",}
+
+]);
+if(answers.userguessnumber===randomNumber){
+console.log("congratulation you guess right number")
+}else{
+    console.log("you guess wrong number")
+}
